ec0918de2f068cc4d96afef0351cbdf2c4f6ae90
carlodaniele <carlo.daniele@gmail.com>
2019-11-24 18:43:41 +0100

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+/* enqueue scripts and style from parent theme */        
+function twentytwenty_styles() {
+	wp_enqueue_style( 'parent', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'twentytwenty_styles');
